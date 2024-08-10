30e8103a294e0c7b81edfe110d530c02f4b58504
notlin4 <121224522+notlin4@users.noreply.github.com>
2024-08-10 16:11:02 +0800

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,6 @@
+winget install -e Microsoft.VCRedist.2015+.x64
+winget install -e EclipseAdoptium.Temurin.8.JDK
+winget install -e EclipseAdoptium.Temurin.17.JDK
+winget install -e EclipseAdoptium.Temurin.21.JDK
+winget install -e PrismLauncher.PrismLauncher
+pause
